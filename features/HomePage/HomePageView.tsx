e53e9ed5216b3a7d01bbab349fb7ebd12d498d3e
Joshua Ohlman <codewriterohs@gmail.com>
2020-04-29 09:30:24 -0400

    Update home page view for better use as a template

--- features/HomePage/HomePageView.tsx
+++ features/HomePage/HomePageView.tsx
@@ -1,4 +1,6 @@
 import React from "react";
 import { ClientPorts } from "../ports";
 
-export default ({ ports }: { ports: ClientPorts }) => <div />;
+export default ({ ports }: { ports: ClientPorts }) => {
+  return <div>TODO</div>;
+};

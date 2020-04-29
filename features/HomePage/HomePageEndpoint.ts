3d51080f27984d8b4c3ce91cbf01dce9e8a01d8e
Joshua Ohlman <codewriterohs@gmail.com>
2020-04-29 19:26:03 -0400

    Rename tings

--- features/HomePage/HomePageEndpoint.ts
+++ features/HomePage/HomePageEndpoint.ts
@@ -1,4 +1,4 @@
-import { ServerPort } from "../../interfaces/serverPort";
+import { ServerPort } from "../../infrastructure/serverPort";
 
 export default class HomePageEndpoint implements ServerPort<string, string> {
   async execute(echo: string) {

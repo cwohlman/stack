13a4beaed15452c736fdfe1b23b8f7c43c7dd99e
Joshua Ohlman <codewriterohs@gmail.com>
2020-04-29 22:43:39 -0400

    Render the home page

--- client/index.tsx
+++ client/index.tsx
@@ -1,1 +1,6 @@
-console.log('hello')
+import React from 'react';
+import ReactDOM from 'react-dom';
+import HomePage from '../features/HomePage/HomePageView';
+
+const root = document.querySelector('#root');
+ReactDOM.render(<HomePage ports={{} as any} />, root);
